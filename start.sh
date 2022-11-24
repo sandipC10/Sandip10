85c4218e3fe9cce5020d7dcb760e06f45b8e573c
sandipC10 <110212502+sandipC10@users.noreply.github.com>
2022-11-24 15:35:32 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/sandipC10/Sandip10 
+  git clone https://github.com/sandipC10/Sandip10/Eva 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Sandip10
+  git clone $UPSTREAM_REPO /Sandip10/Eva
 fi
 cd /Eva
 pip3 install -U -r requirements.txt
