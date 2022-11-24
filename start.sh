e717bb5919eb3ce58bfb9549bfced8f54f15da1a
sandipC10 <110212502+sandipC10@users.noreply.github.com>
2022-11-24 07:28:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/GreyMatterbots/url-auto-delete-shortener-bot /Eva
+  git clone https://github.com/sandipC10/Sandip10 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Eva
+  git clone $UPSTREAM_REPO /Sandip10
 fi
 cd /Eva
 pip3 install -U -r requirements.txt
